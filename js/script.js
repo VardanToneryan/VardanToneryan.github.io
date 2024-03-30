5e9cf4d23d0e2ac450bd326d63c16c5df0b242a7
VardanToneryan <Vardan.toneryan.g@gmail.com>
2024-03-30 18:19:44 +0400

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,28 @@
-let socialBlock = document.getElementById('socialMedias')
-let button = document.getElementById('button')
-socialBlock.style.display = 'none'
-
+let socialBlock = document.getElementById('socialMedias');
+let button = document.getElementById('button');
+let skillListButton = document.getElementById('skillListButton');
+let skillList = document.getElementById('skillList');
+socialBlock.style.display = 'none';
+skillList.style.display = 'none';
 button.addEventListener('click', () => {
     if (socialBlock.style.display == 'none') {
         socialBlock.style.display = 'block'
     } else {
         socialBlock.style.display = 'none'
     }
-});
 
+    if(skillList.style.display == 'table') {
+        skillList.style.display = 'none'
+    }
+});
+skillListButton.addEventListener('click', () => {
+    if (skillList.style.display == 'none') {
+        skillList.style.display = 'table';
+    } else {
+        skillList.style.display = 'none'
+    }
+
+    if(socialBlock.style.display == 'block') {
+        socialBlock.style.display = 'none'
+    }
+})
